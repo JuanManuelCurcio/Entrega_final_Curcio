0ca55f594047c01505644232dc2d5ba5ab56db68
Juan Manuel <juanmanuelcurcio@gmail.com>
2024-10-09 00:42:19 -0300

    AFTER MATRIX AND HEATMAP

--- BehLab/BehLabTools/views.py
+++ BehLab/BehLabTools/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 import pandas as pd
 from django.shortcuts import render
+from BehLab import settings
+import os
 from .forms import csv_upload_form
 from io import TextIOWrapper # para manejar in-out: https://docs.python.org/es/3.10/library/io.html
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 # Create your views here.
@@ -32,3 +36,30 @@ def load_csv_describe_view(req):
         form = csv_upload_form()
 
     return render(req, 'load_csv_describe_view.html', {'form': form})
+
+# LOAD CSV FILE, CORRELATION MATRIX
+
+def load_csv_mcorr_view(req):
+    if req.method == 'POST':
+        form = csv_upload_form(req.POST, req.FILES)
+        if form.is_valid():
+            csv_file = req.FILES['csv_file']
+            df = pd.read_csv(csv_file.file, sep=';',encoding='utf-8')
+            
+            mcorr = df.corr(method='pearson').round(2)
+
+            plt.figure(figsize=(12, 10))
+            graf = sns.heatmap(df.corr('pearson'), annot=True, cmap='magma',annot_kws={"size": 20})
+            graf.set_xticklabels(graf.get_xticklabels(), rotation=45, fontsize=14)
+            graf.set_yticklabels(graf.get_yticklabels(), rotation=0, fontsize=14)
+            plot_dir = os.path.join(settings.MEDIA_ROOT, 'plots') # esto me conviene me parece rearlo en algun la do como fijo como el media root
+            image_path = os.path.join(plot_dir, 'correlation_heatmap.png')
+            plt.savefig(image_path)
+            plt.close()
+
+            return render(req, 'results_mcorr.html', {'graf': os.path.join(settings.MEDIA_URL, 'plots', 'correlation_heatmap.png'),  'mcorr': mcorr.to_html(), 
+            'csv_file': df.to_html(index=False)})
+    else:
+        form = csv_upload_form()
+
+    return render(req, 'load_csv_mcorr_view.html', {'form': form})
